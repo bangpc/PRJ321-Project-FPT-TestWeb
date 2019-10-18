948aeedd6a6b7412cc276a8dbaa215047e5684bd
tadangkhoa1999 <tadangkhoa1999@gmail.com>
2019-10-18 22:13:20 +0700

    -

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -24,7 +24,7 @@ public class DBContext {
 
     /*Insert your other code right after this comment*/
     /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
-    private final String serverName = "DESKTOP-16KB3C8\\SQLEXPRESS";
+    private final String serverName = "localhost";
     private final String dbName = "FPT_Test";
     private final String portNumber = "1433";
     private final String userID = "sa";
